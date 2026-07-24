bf79e984024776c77bcc1336f6cbb95c7cc7b9f2
selvaganesh907 <pselvag1@gmail.com>
2026-07-24 14:39:24 +0530

    first commit

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path("",views.home,name='home'),
     path('vendor/',views.vendor,name='vendor'),
     path('customer/',views.customer,name='customer'),
     path('upload/',views.product_upload,name='upload'),
